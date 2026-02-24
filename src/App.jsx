0b41fd0cd667929cdc7c89f82c06bc7e99733007
anashali01 <saiyedanashali@gmail.com>
2026-02-24 11:27:08 +0530

    form add

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,55 @@
-import React, { useState } from 'react'
-import { Route, Router } from 'react-router-dom'
-import AddEmp from './pages/addEmp'
-import ViewEmp from './pages/viewEmp'
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import AddEmp from "./pages/addEmp";
+import ViewEmp from "./pages/viewEmp";
 
 const App = () => {
-
-  const [employees , setEmployees] = useState({});
-  const [list , setList] = useState([]);
+  const [employees, setEmployees] = useState({});
+  const [list, setList] = useState([]);
+  const [hobbies, setHobbies] = useState([]);
 
   const handleChange = (e) => {
-    const {name , value} = e.target;
-    setEmployees({...employees , [name] : value});
-  }
+    let { name, value, checked } = e.target;
+
+    let newHobby = [...hobbies];
+    if (name == "hobby") {
+      if (checked) {
+        newHobby = [...hobbies, value]; //Set Checked Values
+      } else {
+        newHobby.filter((item) => item != value); //Remove Unchecked Value
+      }
+
+      value = newHobby;
+      setHobbies(newHobby);
+    }
+
+    setEmployees({ ...employees, [name]: value });
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    console.log(list);
-    
-  }
+    console.log(employees);
+    setEmployees({});
+    setHobbies([])
+  };
   return (
     <>
-      <Router>
-        <Route path='/' element={<AddEmp/>}/>
-        <Route path='/viewEmp' element={<ViewEmp/>}/>
-      </Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <AddEmp
+              handleChange={handleChange}
+              handleSubmit={handleSubmit}
+              employees={employees}
+              hobbies={hobbies}
+            />
+          }
+        />
+        <Route path="/viewEmp" element={<ViewEmp />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/pages/AddEmp.jsx
+++ src/pages/AddEmp.jsx
@@ -1,57 +1,115 @@
 import React from "react";
 
-const AddEmp = () => {
+const AddEmp = ({ handleChange, handleSubmit, employees , hobbies}) => {
+  const cities = ["Surat", "Ahmadabad", "Vadodara", "Chennai"];
   return (
     <>
-      <form action="" method="post">
-        <div>
-          <label htmlFor="name">Name :</label>
-          <input type="text" />
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-5">
+            <h1 className="text-center fw-bolder mb-3">Add Employee Data</h1>
+            <form action="" method="post" onSubmit={handleSubmit}>
+              <div className="mb-3">
+                <label htmlFor="name" className="form-label">
+                  Name :
+                </label>
+                <input type="text" name="name" onChange={handleChange} className="form-control" value={employees.name || ''}/>
+              </div>
+              {/* Email  */}
+              <div>
+                <label className="form-label" htmlFor="email">
+                  Email :
+                </label>
+                <input type="email" name="email" onChange={handleChange} className="form-control" value={employees.email || ''}/>
+              </div>
+              {/* Password  */}
+              <div>
+                <label className="form-label" htmlFor="password">
+                  Password :
+                </label>
+                <input type="password" name="password" onChange={handleChange} className="form-control" value={employees.password || ''}/>
+              </div>
+              {/* Gender  */}
+              <div className="mt-3">
+                <label className="form-label me-2" htmlFor="gender">
+                  Gender
+                </label>
+                <input
+                  type="radio"
+                  name="gender"
+                  id="male"
+                  value={"male"}
+                  checked={employees.gender == "male"}
+                  onChange={handleChange}
+                />
+                <label className="form-label me-2" htmlFor="gender">
+                  Male
+                </label>
+                <input
+                  type="radio"
+                  name="gender"
+                  id="female"
+                  value={"female"}
+                  checked={employees.gender == "female"}
+                  onChange={handleChange}
+                />
+                <label className="form-label" htmlFor="gender">
+                  Female
+                </label>
+              </div>
+              {/* Hobby  */}
+              <div className="mt-3">
+                <label className="form-label me-2" htmlFor="hobby">
+                  Hobby :
+                </label>
+                <input type="checkbox" onChange={handleChange} checked={hobbies?.includes('dancing')} name="hobby" value='dancing' id="dancing" />
+                <label className="form-label me-2" htmlFor="dancing">
+                  Dancing
+                </label>
+                <input type="checkbox" onChange={handleChange} checked={hobbies?.includes('singing')} name="hobby" value='singing' id="singing" />
+                <label className="form-label me-2" htmlFor="singing">
+                  Singing
+                </label>
+                <input type="checkbox" onChange={handleChange} checked={hobbies?.includes('reading')} name="hobby" value='reading' id="reading" />
+                <label className="form-label" htmlFor="reading">
+                  Reading
+                </label>
+              </div>
+              {/* City  */}
+              <div className="mt-3">
+                <label className="form-label me-2" htmlFor="city">
+                  City :
+                </label>
+                <select name="city" id="city" onChange={handleChange}>
+                  <option value="" selected >
+                    Select City
+                  </option>
+                  {cities.map((c) => (
+                    <option value={c} selected={employees.city === c}>{c}</option>
+                  ))}
+                </select>
+              </div>
+              {/* TextArea  */}
+              <div className="mt-3">
+                <label htmlFor="address" className="form-label">
+                  Address :
+                </label>
+                <textarea
+                  name="address"
+                  id="address"
+                  className="form-control"
+                  value={employees.address || ''}
+                  onChange={handleChange}
+                />
+              </div>
+              {/* Button  */}
+              <button type="submit" className="btn btn-info mt-3">
+                Add Employee
+              </button>
+            </form>
+          </div>
         </div>
-        {/* Email  */}
-        <div>
-          <label htmlFor="email">Email :</label>
-          <input type="email" />
-        </div>
-        {/* Password  */}
-        <div>
-          <label htmlFor="password">Password :</label>
-          <input type="password" />
-        </div>
-        {/* Gender  */}
-        <div>
-          <label htmlFor="gender">Gender</label>
-          <input type="radio" name="gender" id="male" value={"male"} />
-          <label htmlFor="gender">Male</label>
-          <input type="radio" name="gender" id="female" value={"female"} />
-          <label htmlFor="gender">Female</label>
-        </div>
-        {/* Hobby  */}
-        <div>
-          <label htmlFor="hobby">Hobby :</label>
-          <input type="checkbox" name="dancing" id="dancing" />
-          <label htmlFor="dancing">Dancing</label>
-          <label htmlFor="hobby">Hobby :</label>
-          <input type="checkbox" name="singing" id="singing" />
-          <label htmlFor="singing">Singing</label>
-          <label htmlFor="hobby">Hobby :</label>
-          <input type="checkbox" name="reading" id="reading" />
-          <label htmlFor="reading">Reading</label>
-        </div>
-        {/* City  */}
-        <div>
-          <select name="city" id="city">
-            <option value="" selected disabled>
-              Select City
-            </option>
-            <option value="delhi">Delhi</option>
-            <option value="mumbai">Mumbai</option>
-            <option value="bangalore">Bangalore</option>
-          </select>
-        </div>
-        {/* Button  */}
-        <button type="submit">Add Employee</button>
-      </form>
+      </div>
     </>
   );
 };
